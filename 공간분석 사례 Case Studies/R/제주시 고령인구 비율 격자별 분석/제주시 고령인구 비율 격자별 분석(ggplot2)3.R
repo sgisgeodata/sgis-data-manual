0eb97cc04a50c91c4e2940f0f6e6c064a782bdbc
sgisgeodata <sgisgeodata@gmail.com>
2026-07-15 14:16:08 +0900

    R 제주시 예제 점검

--- 공간분석 사례 Case Studies/R/제주시 고령인구 비율 격자별 분석/제주시 고령인구 비율 격자별 분석(ggplot2)3.R
+++ 공간분석 사례 Case Studies/R/제주시 고령인구 비율 격자별 분석/제주시 고령인구 비율 격자별 분석(ggplot2)3.R
@@ -14,7 +14,7 @@ map_2024 <- jeju_elderly_ratio_map(base_year="2024")
 map_grid <- grid.arrange(map_2000, map_2010, map_2024, ncol=3)
 
 # 지도를 그림파일로 저장
-file_path <- paste(prj_dir, "제주도 고령인구 비율 격자별 분석_GRID.png", sep="/")
+file_path <- paste(prj_dir, "제주도 고령인구 비율 격자별 분석.png", sep="/")
 ggsave(map_grid, filename=file_path)
 # 너비, 높이는 인치 단위로 지정 가능
 
